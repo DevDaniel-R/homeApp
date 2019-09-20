796858db4bdfc696a9bebc02cb7ddb81d2010fd8
Daniel Rodriguez <drodriguez661@gmail.com>
2019-09-19 19:28:09 -0500

    added all js and figured out how to edit text live. next intergrate weather info?

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ greeting = document.getElementById('greeting'),
 name = document.getElementById('name'),
 focus = document.getElementById('focus');
 
+//Options
+const showAmPm = true;
+
 // Show Time 
 function showTime() {
   let today = new Date(),
@@ -18,7 +21,7 @@ function showTime() {
   hour = hour % 12 || 12;
 
   //Output Time
-  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} ${showAmPm ? amPm : ''}`;
 
   setTimeout(showTime, 1000);
 }
@@ -53,6 +56,59 @@ function setBgGreet() {
   }
 }
 
+// Get Name 
+function getName() {
+  if (localStorage.getItem('name') === null) {
+    name.textContent = '[Enter Name]';
+  } else {
+    name.textContent = localStorage.getItem('name');
+  }
+}
+
+//set Name
+function setName(e) {
+  if (e.type === 'keypress') {
+    //Make sure enter is pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem('name', e.target.innerText);
+      name.blur();
+    }
+  } else {
+    localStorage.setItem('name', e.target.innerText);
+  }
+}
+
+// Get Focus 
+function getFocus() {
+  if (localStorage.getItem('focus') === null) {
+    focus.textContent = '[Enter Focus]';
+  } else {
+    focus.textContent = localStorage.getItem('focus');
+  }
+}
+
+//set Focus
+function setFocus(e) {
+  if (e.type === 'keypress') {
+    //Make sure enter is pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem('focus', e.target.innerText);
+      focus.blur();
+    }
+  } else {
+    localStorage.setItem('focus', e.target.innerText);
+  }
+}
+
+
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
+
   //run
   showTime();
   setBgGreet();
+  getName();
+  getFocus();
